f4fca861c423958d8f08b7b5cec86a4bff6e8879
bintabaldeh <47475073+bintabaldeh@users.noreply.github.com>
2019-07-31 03:32:53 +0900

    testing javaScript with html

---
+++ javaScript/main.js
@@ -0,0 +1,16 @@
+var names = [
+  {
+    username: "Moe",
+    password: "codify",
+  },
+  {
+    username: "lamin",
+    password: "read",
+  },
+]
+
+function getInfo() {
+  var username = document.getElementById("username").value
+  var password = document.getElementById("password").value
+  console.log("you're username is " + username + " and your password is " + password);
+}
